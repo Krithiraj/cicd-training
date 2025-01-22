b1a221e093f13bc50a621b10759959105cd6ea53
Ubuntu <labuser@katalon.hvulif0mqrpejbull0tbjf2lcg.jgax.internal.cloudapp.net>
2025-01-22 05:07:06 +0000

    Pushing local changes

---
+++ spring-boot-app/src/main/java/com/example/springapp/api/MyRestApi.java
@@ -0,0 +1,15 @@
+package com.example.springapp.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestApi {
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello from upgrade";
+    }
+
+
+}
